f59fc3dca2ac8ff1851af17935dab844ca33d649
Emanuele Caprioli <emanuele@dhcp-10-22-42-125.wlan.ntnu.no>
2018-11-06 13:52:34 +0100

    updated sensob linefollowing

--- sensob.py
+++ sensob.py
@@ -4,14 +4,13 @@ from plab.reflectance_sensors import *
 class Sensob:
 
     def __init__(self, sensors):
-        self.data = None
-        self.sensors = sensors
+        self.value = None
+        self.sensors = sensors #1 sensor or list of used sensors
 
     def update(self):
         for sensor in self.sensors:
             sensor.reset()
-            sensor.update()
-            sensor.get_value()
+            sensor.update
 
     def reset(self):
         for sensor in self.sensors:
@@ -19,12 +18,20 @@ class Sensob:
             pass
         
 
-class IR_Array_Sensob(Sensob):
+class LineFollowingSensob(Sensob):
     
     def __init__(self):
         reflectance_board = ReflectanceSensors()
-        super(IR_Array_Sensob, self).__init__([reflectance_board])
+        super(LineFollowingSensob, self).__init__(reflectance_board)
 
     def update(self):
         super().update()
-        
+        red_values = self.sensors.get_value()
+        values_and_sensors = {}
+        for i in range(0, len(red_values)):
+            values_and_sensors[red_values[i]] = i
+        red_values.sort(reverse=True)
+        self.value = (values_and_sensors.get(red_values[0]), values_and_sensors.get(red_values[3]))
+
+
+
