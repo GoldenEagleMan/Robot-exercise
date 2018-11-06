421e0567d1fd43600b1807e410daec842d5a5e9b
Emanuele Caprioli <emanuele@dhcp-10-22-42-125.wlan.ntnu.no>
2018-11-06 13:19:35 +0100

    this is for eivind to see

--- sensob.py
+++ sensob.py
@@ -1,5 +1,5 @@
 from plab.ultrasonic import *
-
+from plab.reflectance_sensors import *
 
 class Sensob:
 
@@ -17,3 +17,14 @@ class Sensob:
         for sensor in self.sensors:
             sensor.reset()
             pass
+        
+
+class IR_Array_Sensob(Sensob):
+    
+    def __init__(self):
+        reflectance_board = ReflectanceSensors()
+        super(IR_Array_Sensob, self).__init__([reflectance_board])
+
+    def update(self):
+        super().update()
+        
