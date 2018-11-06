4fcc22e0544e3598bcd891b4e58840cb4d12416f
Emanuele Caprioli <emanuele@dhcp-10-22-42-125.wlan.ntnu.no>
2018-11-06 12:54:24 +0100

    hello

--- BBCON.py
+++ BBCON.py
@@ -4,9 +4,9 @@ class BBCON:
         self.behaviors = behaviors
         self.sensobs = sensobs
         self.motobs = motobs
-        self.active_behaviors = None
+        self.active_behaviors = []
+        self.active_sensobs = []
         self.arbitrator = arbitrator
-        self.arbitrator.BBCON = self
 
     def add_behavior(self, behavior):
         self.behaviors.append(behavior)
@@ -19,27 +19,34 @@ class BBCON:
     def activate_behavior(self, behavior):
         if behavior not in self.active_behaviors:
             self.active_behaviors.append(behavior)
+        for sensob in behavior.sensobs:
+            if sensob not in self.active_sensobs:
+                self.active_sensobs.append(sensob)
         pass
 
     def deactivate_behavior(self, behavior):
         if behavior in self.active_behaviors:
             self.active_behaviors.remove(behavior)
+        for sensob in behavior.sensobs:
+            if sensob in self.active_sensobs:
+                self.active_sensobs.remove(sensob)
+
         pass
 
     def run_one_timestep(self):
         #update all sensob
-        for sensob in self.sensobs:
+        for sensob in self.active_sensobs:
             sensob.update()
         #update all behaviors
         for behavior in self.behaviors:
             behavior.update()
-        #invoke arbitrar.
+        #invoke arbitrator.
         self.arbitrator.choose_action() #can be it must be passed a list of behaviors
         #update all motors
             #not yet implemed
         #Wait
             # not yet implemed
         #reset all sensob
-        for sensob in self.sensobs:
+        for sensob in self.active_sensobs:
             sensob.reset()
 
