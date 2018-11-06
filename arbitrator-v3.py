76fd129bab4adbb30679d21ee4400e2b7ec137fb
eivindkopperud <43027083+eivindkopperud@users.noreply.github.com>
2018-11-06 14:00:57 +0100

    Update and rename arbitrator-v2.py to arbitrator-v3.py

--- arbitrator-v3.py
+++ arbitrator-v3.py
@@ -3,8 +3,9 @@ class Arbitrator:
         self.bbcon = bbcon
 
     def choose_action(self):
+        active_behaviors = filter(lambda x: x.active_flag == True, self.bbcon.behaviors)
         prioritized_behavior = None
-        for behavior in self.bbcon.behaviors:
+        for behavior in active_behaviors:
             if prioritized_behavior == None or prioritized_behavior.weigth < behavior.weigth:
                 prioritized_behavior = behavior
         return (prioritized_behavior.motor_recommendations, prioritized_behavior.halt_request)
