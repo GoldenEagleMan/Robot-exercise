7b1e6f1929ed11a142774fe09065c623a7242a09
Emanuele Caprioli <emanuele@dhcp-10-22-8-122.wlan.ntnu.no>
2018-11-09 14:41:20 +0100

    testing

--- motob.py
+++ motob.py
@@ -11,14 +11,14 @@ class Motob:
         command = motor_recommendation[0]
         angle = motor_recommendation[1]
         if command == "goForward":
-            self.motors.forward(self.angle_to_speed(angle))
+            self.motors.forward(self.angle_to_speed(angle), 3)
         elif command == "turn":
             if angle > 0:
-                self.motors.right(self.angle_to_speed(angle))
+                self.motors.right(self.angle_to_speed(angle), 3)
             else:
-                self.motors.left(self.angle_to_speed(angle))
+                self.motors.left(self.angle_to_speed(angle), 3)
         elif command == "goBackward":
-            self.motors.backward(self.angle_to_speed(angle))
+            self.motors.backward(self.angle_to_speed(angle), 3)
         elif command == "stopAllMotors":
             self.motors.stop()
         pass
