a92ce877cce7c51174e6804795c63b00225a4cb9
Emanuele Caprioli <emanuele@dhcp-10-22-8-122.wlan.ntnu.no>
2018-11-09 10:56:40 +0100

    created main and robot class

--- robot.py
+++ robot.py
@@ -1,7 +1,39 @@
 from BBCON import BBCON
-from behavior import Behavior
-from sensob import Sensob
+from behavior import *
+from sensob import *
 from motob import Motob
 from arbitrator import Arbitrator
 
 
+class Robot:
+
+    def __init__(self):
+        arbitrator = Arbitrator(self)
+        motob = Motob()
+        self.controller = BBCON(Robot.build_behavior_list(), Robot.build_sensob_list(), motob, arbitrator)
+
+    def run(self):
+        run = True
+        while run:
+            run = self.controller.run_one_timestep()
+
+    @staticmethod
+    def build_sensob_list():
+        cameraSensob = CameraSensob()
+        irSensobRight = IRSensobRight()
+        irSensobLeft = IRSensobLeft()
+        endpointDetectionSensob = EndpointDetectionSensob()
+        lineDetectionSensob = LineDetectionSensob()
+        lineFollowingSensob = LineFollowingSensob()
+        ultrasoundSensob = UltrasoundSensob()
+
+        return [cameraSensob, irSensobRight, irSensobLeft, endpointDetectionSensob, lineDetectionSensob,
+                   lineFollowingSensob, ultrasoundSensob]
+    @staticmethod
+    def build_behavior_list():
+        collisionDetectionBehavior = CollisionDetection()
+        goAroundObjectBehavior = GoAroundObject()
+        followLineBehavior = FollowLine()
+        redDetectorBehavior = RedDetector()
+
+        return [collisionDetectionBehavior, goAroundObjectBehavior, followLineBehavior, redDetectorBehavior]
