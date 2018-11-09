18dc1dbe668ca4936df173dc9a8edb40ed783d4b
Emanuele Caprioli <emanuele@dhcp-10-22-8-122.wlan.ntnu.no>
2018-11-09 13:32:09 +0100

    fixes

--- arbitrator.py
+++ arbitrator.py
@@ -6,6 +6,6 @@ class Arbitrator:
         #active_behaviors = filter(lambda x: x.active_flag == True, self.bbcon.behaviors)
         prioritized_behavior = None
         for behavior in self.bbcon.active_behaviors:
-            if prioritized_behavior == None or prioritized_behavior.weight < behavior.weight:
+            if prioritized_behavior is None or prioritized_behavior.weight < behavior.weight:
                 prioritized_behavior = behavior
         self.bbcon.run_behavior = (prioritized_behavior.motor_recommendations, prioritized_behavior.halt_request)
